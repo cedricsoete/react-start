3c3dbc57b68602e5cb6db51bd241204213574adc
Cedric Soete <cedric.soete@hotmail.fr>
2021-02-13 11:08:44 +0100

    Mock des l'API's et Serveur d'images

---
+++ tools/fileApi/fileServer.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const upload = require("express-fileupload");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(upload());
+
+app.post("/upload", (req, res) => {
+  console.log(req);
+  if (req.files) {
+    const file = req.files.file;
+    const filename = req.body.name;
+    file.mv("./tools/pictures/" + filename, (err) => {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.status(200).send("File Uploaded");
+      }
+    });
+  }
+});
+
+app.listen(8082, () => {
+  console.log("fileServer started on localhost:8082");
+});
